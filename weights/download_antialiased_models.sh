2ba1e77bd52bdb3486ab1efe7ebefeddabea9b89
Richard Zhang <rizhang@rizhangs-MacBook-Pro.local>
2019-05-16 16:57:18 -0700

    download model script

--- weights/download_antialiased_models.sh
+++ weights/download_antialiased_models.sh
@@ -1,4 +1,14 @@
 
-wget https://www.dropbox.com/s/vslb4znzvjt0nei/alexnet_lpf2.pth.tar?dl=0 -O ./weights/
-wget https://www.dropbox.com/s/4ls28pq2teqijxg/alexnet_lpf3.pth.tar?dl=0 -O ./weights/
-wget https://www.dropbox.com/s/8ikxtsg4e395zvw/alexnet_lpf5.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/fn1lvadzkrvm9k2/alexnet_lpf2.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/qnirjsi1kggx298/alexnet_lpf3.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/ck8h8soz8adf65f/alexnet_lpf5.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/a3o89q88mvcsofg/densenet121_lpf2.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/5w2da7227e5147n/densenet121_lpf3.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/xyygwancsm8obk4/densenet121_lpf5.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/cl8d7dixii9x84f/resnet50_lpf2.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/6qx4izyggfn4yov/resnet50_lpf3.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/cmxv92f0b0cg52u/resnet50_lpf5.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/e9cv1ephmlne4up/vgg16_lpf2.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/b772puthmxvs2s4/vgg16_lpf3.pth.tar?dl=0 -O ./weights/
+wget https://www.dropbox.com/s/z80jk7hjb389ap6/vgg16_lpf5.pth.tar?dl=0 -O ./weights/
+
