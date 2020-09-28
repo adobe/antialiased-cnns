5b8805b7d5e4ceed74a499282c562647d4d2a88a
Richard Zhang <rizhang@rizhangs-MacBook-Pro.local>
2020-09-28 15:01:15 -0700

    copy buffers

--- antialiased_cnns/util.py
+++ antialiased_cnns/util.py
@@ -14,8 +14,6 @@ def copy_params(src_model, dest_model):
         for params in zip(src_params, dest_params):
             params[1][...] = params[0][...]
 
-    return dest_model
-
 def copy_buffers(src_model, dest_model):
 	src_buffers = list(src_model.buffers())
 	dest_buffers = list(dest_model.buffers())
@@ -31,10 +29,6 @@ def copy_buffers(src_model, dest_model):
 		with torch.no_grad():
 			dest_buffers[cc-1][...] = buffer[...]
 
-	return dest_model
-
 def copy_params_buffers(src_model, dest_model):
 	copy_params(src_model, dest_model)
 	copy_buffers(src_model, dest_model)
-
-	return dest_model
