b22295177ac525d218aa2dd447c53d58a4500d6b
Richard Zhang <rizhang@rizhangs-MacBook-Pro.local>
2020-10-20 11:06:03 -0700

    serialization

--- antialiased_cnns/resnet.py
+++ antialiased_cnns/resnet.py
@@ -43,7 +43,7 @@ from antialiased_cnns import *
 
 __all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152',
            'resnext50_32x4d', 'resnext101_32x8d',
-           'wide_resnet50_2','wide_resnet101_2']
+           'wide_resnet50_2', 'wide_resnet101_2']
 
 
 model_urls = {
--- main.py
+++ main.py
@@ -350,13 +350,13 @@ def main_worker(gpu, ngpus_per_node, args):
         print("=> saving 'deparallelized' weights [%s]"%args.save_weights)
         # TO-DO: automatically save this during training
         if args.gpu is not None:
-            torch.save({'state_dict': model.state_dict()}, args.save_weights)
+            torch.save({'state_dict': model.state_dict()}, args.save_weights, _use_new_zipfile_serialization=True)
         else:
             if(args.arch[:7]=='alexnet' or args.arch[:3]=='vgg'):
                 model.features = model.features.module
-                torch.save({'state_dict': model.state_dict()}, args.save_weights)
+                torch.save({'state_dict': model.state_dict()}, args.save_weights, _use_new_zipfile_serialization=True)
             else:
-                torch.save({'state_dict': model.module.state_dict()}, args.save_weights)
+                torch.save({'state_dict': model.module.state_dict()}, args.save_weights, _use_new_zipfile_serialization=True)
         return
 
     if args.evaluate:
